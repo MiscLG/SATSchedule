70b14b98906d4190cc315ef04051113a0e9aaa86
Luis F. Garcia Cerecedo <luisfergar99@gmail.com>
2021-12-10 01:07:40 -0800

    first attempt at schedule resolution

--- src/Schedule.py
+++ src/Schedule.py
@@ -20,10 +20,10 @@ class Employee():
 
 
 class Team():
-    name = ""
     members = []
 
     def __init__(self, name, initial_members):
+        self.name = name
         for member in initial_members:
             self.members.append(member)
 
@@ -43,12 +43,11 @@ class WorkDay():
 class Job():
     length_sec = 0
     date = None
-    address = None
-    team = Team
+    team = None
 
     def __init__(self, length_hr, address):
-        self.length_sec = length
-        self.location = address
+        self.length_sec = length_hr
+        self.address = address
 
     def set_date(self, date):
         self.date = date
@@ -68,57 +67,156 @@ class Schedule():
         assignments = {} dict mapping jobs to teams and days
         var_dict= {} map of cnf object to its corresponding digit in the formula
         SAT_fomula = CNF()
+
+
+    Variables:
+     Teams: T_i {true: if team is working, false: otherwise }
+     Jobs: J_ij {true if team is assigned to job j, false: otherwise}
+     Days: D_ijk {true: if team is assigned a job j on day k, false: otherwise}
+    Constraints:
+    Jobs must be assigned to exactly 1 team, Jobs take at most one full workday:
+    Jobs in the same day must be completed in the duration of the including travel time workday.
+    REACH -Teams cannot be assigned more than 2 jobs more than the rest at any point in the pay period.
     """
+    class CNF_Var:
+        """
+            cnf_ix is the index used by the schedule cnf formula
+            category is the type of variable [T, J, D]
+            mapping contains indexes from each list to the item of the variable
+        """
+
+        def __init__(self, cnf_ix, category, mapping):
+            self.cnf_ix = cnf_ix
+            self.category = category
+            self.mapping = mapping
+            self.sublist = []
+
     pay_period = []
     jobs = []
     teams = []
-    assignments = {}
-    var_dict = []
-    SAT_fomula = CNF()
+    cnf_map = []
+    cnf_vars = []
+    SAT_formula = []
 
-    def __init__(self, period_start=None, period_length=15, workday_len=8, **kwargs):
-        if kwargs["pay_period"]:
+    def __init__(self, period_start=None, period_length=5, workday_len=8, **kwargs):
+        if "pay_period" in kwargs:
             self.pay_period = kwargs["pay_period"]
         else:
             if not period_start:
                 period_start = datetime.datetime.today()
             std_pay_period = [WorkDay(workday_len, date)
                               for date in daterange(period_length, period_start)]
+            self.pay_period = std_pay_period
+
+    def unmap_team(self, team_ix):
+        return self.cnf_vars[self.cnf_map[team_ix]-1]
+
+    def unmap_job(self, team_ix, job_ix):
+        return self.cnf_vars[self.unmap_team(team_ix).sublist[job_ix]-1]
+
+    def unmap_day(self, team_ix, job_ix, day_ix):
+        return self.cnf_vars[self.unmap_job(team_ix, job_ix).sublist[day_ix]-1]
 
     def curr_cnf_num(self):
-        return len(var_dict) + 1
+        return len(self.cnf_vars) + 1
 
-    def job_cnf_clauses(self, job):
-        # TODO: model as a graph
+    def add_var(self, category, mapping):
+        cnf_num = self.curr_cnf_num()
+        var = self.CNF_Var(cnf_num, category, mapping)
+        self.cnf_vars.append(var)
+        return cnf_num
 
-        pass
+    def make_inner_vars(self, i, j):
+        J_ij = self.add_var("J", {"job": j, "team": i})
+        self.unmap_team(i).sublist.append(J_ij)
+        for k, day in enumerate(self.pay_period):
+            D_ijk = self.add_var("D", {"day": k, "job": j, "team": i})
+            self.unmap_job(i, j).sublist.append(D_ijk)
 
     def add_job(self, job):
-        job_index = len(self.jobs)
-        job_cnf = curr_cnf_num()
-        self.jobs.append({"obj": job, "cnf_ix": job_cnf})
-        self.var_dict.append({"category": "job", "index": job_index})
+        j = len(self.jobs)
+        self.jobs.append(job)
+        # add avrs for this job
+        for i, team in enumerate(self.teams):
+            self.make_inner_vars(i, j)
 
     def add_team(self, team):
-        team_index = len(self.teams)
-        team_cnf = curr_cnf_num()
-        self.teams.append({"obj": team, "cnf_ix": team_cnf})
-        self.var_dict.append({"category": "team", "index": team_index})
-        # we would reassign here if we were accounting for fair, weighted, distribution of work
+        i = len(self.teams)
+        self.teams.append(team)
+        T_i = self.add_var("T", {"team": i})
+        self.cnf_map.append(T_i)
+        # add vars for this team
+        for j, job in enumerate(self.jobs):
+            self.make_inner_vars(i, j)
 
-    def humanize_result(self, result):
+    def encode_full_cnf(self):
+        # assumes mapping is correct and complete
+        # this assumes one job per day
+        clauses = []
+        for team in self.cnf_map:
+            # having this team implies variables containing these teams
+            T_i = self.cnf_vars[team-1]
+            i = T_i.cnf_ix
+            team_per_job = [-i]
+            for job in T_i.sublist:
+                # print(job)
+                J_ij = self.cnf_vars[job-1]
+                j = J_ij.cnf_ix
+                job_per_team = [-j, i]
+                clauses.append(job_per_team)
+                day_per_job = [-j]
+                team_per_job.append(j)
+                for day in J_ij.sublist:
+                    # do [-D_ij, J_ij][-D_ij,J_ij] one clause per job
+                    # having this day implies no other job has this day
+                    # TODO: make this account for multiple jobs in a day
+                    D_ijk = self.cnf_vars[day-1]
+                    k = D_ijk.cnf_ix
+                    job_per_day = [-k, j]
+                    clauses.append(job_per_day)
+                    # also
+                    # do [-JT_ij,DJT_ij,DJT_ij] one item in clause per job
+                    # having this job implies no other day has this job
+                    day_per_job.append(k)
+                clauses.append(day_per_job)
+            clauses.append(team_per_job)
+
+        self.SAT_formula = clauses
+
+    def resolve(self):
+        self.encode_full_cnf()
+        print(self.SAT_formula)
+        print([var.cnf_ix for var in self.cnf_vars])
+        g = MinisatGH(bootstrap_with=self.SAT_formula)
+        print(g.solve())
+        res = g.get_model()
+        print(res)
+        return self.humanize(res)
+
+    def humanize(self, result):
         if None:
             return "Unsatisfiable"
         res_obj = {}
-        for var in result:
-            val = var > 0
-            data = var_dict[Math.abs(var)]
-            if data["category"] == "team":
-                key = self.teams[data["index"]]["obj"].name
-                res_obj[key] = val
-            elif data["category"] == "job":
-                key = self.jobs[data["index"]]["obj"].address
-                res_obj[key] = val
+        for ix in result:
+            val = ix > 0
+            var = self.cnf_vars[abs(ix)-1]
+            # if var.category == "T":
+            #     key = self.teams[var.mapping["team"]].name
+            #     res_obj[key] = var.mapping
+            # elif var.category == "J":
+            #     key = self.jobs[var.mapping["team"]].address
+            #     res_obj[key] = var.mapping
+            # else:
+            if var.category == "D":
+                key = ix
+                job = self.jobs[var.mapping["job"]].address
+                team = self.teams[var.mapping["team"]].name
+                day = var.mapping["day"]
+                res_obj[key] = {
+                    "job": job,
+                    "team": team,
+                    "day": day,
+                }
 
         return res_obj
 
@@ -128,7 +226,12 @@ if __name__ == "__main__":
     formula = CNF()
     formula.append([-1, 2])
 
-    g = MinisatGH(bootstrap_with=formula)
-    print(g.solve())
-    res = g.get_model()
-    print(g.get_model())
+    test = Schedule(period_length=1)
+    team1 = Team("A", [Employee("Luis"), Employee("Leo")])
+    team2 = Team("B", [Employee("Eva"), Employee("Mar")])
+    test.add_team(team1)
+    # test.add_team(team2)
+    test.add_job(Job(3, "1 LMU Drive"))
+    test.add_job(Job(3, "1062 Durness"))
+
+    print(test.resolve())
